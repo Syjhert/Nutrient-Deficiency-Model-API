2ece93ca84c8f1a1c257dc06c2dde1d71d8faabc
Syjhert <robillos.jorashjonathan3@gmail.com>
2025-04-27 20:30:50 +0800

    basic streamlit app

---
+++ streamlit/streamlit_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image as keras_image
+
+model = load_model('rice_deficiency_model.h5')
+
+classes = ['Nitrogen(N)', 'Phosphorus(P)', 'Potassium(K)']
+
+def preprocess_image(image_data):
+    img = image_data.resize((224, 224))
+    img_tensor = keras_image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+    return img_tensor
+
+st.title("Rice Halation Basic Streamlit App")
+
+uploaded_file = st.file_uploader("Upload a rice leaf image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
+
+    if st.button("Predict"):
+        processed_image = preprocess_image(image)
+        prediction = model.predict(processed_image)
+        class_idx = np.argmax(prediction[0])
+        class_label = classes[class_idx]
+        confidence = float(prediction[0][class_idx])
+
+        st.header(f"Predicted Class: {class_label} \nConfidence: {confidence*100:.2f}%")
