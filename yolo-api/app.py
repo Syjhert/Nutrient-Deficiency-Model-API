131b8a90732c22a5bc7a6dd5b1dc7e9ec888bcea
Syjhert <robillos.jorashjonathan3@gmail.com>
2025-05-04 22:42:11 +0800

    yolo model for banana leaves

---
+++ yolo-api/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from ultralytics import YOLO
+from PIL import Image
+import os
+
+app = Flask(__name__)
+CORS(app)
+model = YOLO('yolo_banana_nutrient_best.pt')
+classes = ['boron', 'calcium', 'healthy', 'iron', 'magnesium', 'manganese', 'potassium', 'sulfur', 'zinc']
+
+@app.route('/keepalive', methods=['GET'])
+def api_health():
+    return jsonify(Message="Success"), 200
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if "image" not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    image_file = request.files["image"]
+    image = Image.open(image_file.stream).convert("RGB")
+
+    # Run classification
+    results = model(image)
+
+    pred = results[0].probs.top1
+    confidence = results[0].probs.data[pred].item()
+
+    return jsonify({
+        "prediction": classes[pred],
+        "confidence": round(confidence, 4)
+    })
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ yolo-api/request.py
@@ -0,0 +1,19 @@
+import requests 
+# Define the URL of your Flask API 
+url = 'http://127.0.0.1:5000/predict' 
+ 
+# Define the input data(image) 
+image_path = 'potassium.jpg'
+
+# Make the request while the file is still open
+with open(image_path, "rb") as f:
+    files = {"image": (image_path, f, "image/jpeg")}
+    response = requests.post(url, files=files)
+
+    # Handle response here while still inside the `with` block
+    if response.status_code == 200:
+        print("✅ Success")
+        print(response.json())
+    else:
+        print(f"❌ API Request Failed with Status Code: {response.status_code}")
+        print("Response Content:", response.text)
